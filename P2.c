fd3e3a011e4f7d2e68a47eb32b1868565b4f3eac
Santiago Gonzalez <santiriver2009@gmail.com>
2023-07-13 12:39:38 -0300

    buscar tarea por palabra

--- P2.c
+++ P2.c
@@ -153,3 +153,40 @@ void mostrarTareasPendientes(Tarea** tareasPendientes, int cantTareas)
         }        
     }
 }
+
+void BuscarTarea(Tarea** tareasPendientes, Tarea** tareasRealizadas, int cantTareas)
+{
+    int bandera=0;
+    char *palabra=(char*) malloc(sizeof(char)*50);
+
+    printf("\n\t=======Busqueda de tareas=======\n");
+    printf("Palabra clave buscada: ");
+    scanf("%s",palabra);
+    printf("\n");
+    for (int i = 0; i < cantTareas; i++)
+    {
+        if (tareasPendientes[i] && strstr(tareasPendientes[i]->Descripcion,palabra))
+        {
+            printf("---Tarea %d: \n",tareasPendientes[i]->TareaID);
+            printf("Descripcion %s\n",tareasPendientes[i]->Descripcion);
+            printf("Duracion: %d\n",tareasPendientes[i]->Duracion);
+            printf("Estado: Pendiente\n");
+            bandera=1;
+
+        }
+        else if (tareasRealizadas[i] && strstr(tareasRealizadas[i]->Descripcion,palabra))
+        {
+            printf("---Tarea %d: \n",tareasRealizadas[i]->TareaID);
+            printf("Descripcion %s\n",tareasRealizadas[i]->Descripcion);
+            printf("Duracion: %d\n",tareasRealizadas[i]->Duracion);
+            printf("Estado: Realizada\n");
+            bandera=1;
+        }
+
+
+    }
+    if (bandera==0)
+    {
+        printf("No existe ninguna tarea asociada a esa palabra\n");
+    }
+}
